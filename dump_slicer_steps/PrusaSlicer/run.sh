2aa78cde91cf9a424db594c652e759d24a33241e
Javad Asgari Shafique <me@javad.sh>
2025-02-17 01:28:25 +0100

    .

--- dump_slicer_steps/PrusaSlicer/run.sh
+++ dump_slicer_steps/PrusaSlicer/run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 pushd slicer-src
-SSL_CERT_FILE=/etc/ssl/certs/ca-certificates.crt xvfb-run /prusa-slicer
+SSL_CERT_FILE=/etc/ssl/certs/ca-certificates.crt xvfb-run ./prusa-slicer
 popd
 
 cp ./slicer-src/*.json ./slicer-out
