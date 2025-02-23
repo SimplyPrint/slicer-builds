6bb90e163acf8a14446a3498a3a68ed08ff20219
Javad Asgari Shafique <me@javad.sh>
2025-02-23 23:35:37 +0100

    Fix prusa slicer runner (binary is in src directory..)

--- dump_slicer_steps/PrusaSlicer/run.sh
+++ dump_slicer_steps/PrusaSlicer/run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 pushd slicer-src
-SSL_CERT_FILE=/etc/ssl/certs/ca-certificates.crt xvfb-run ./build/prusa-slicer
+SSL_CERT_FILE=/etc/ssl/certs/ca-certificates.crt xvfb-run ./build/src/prusa-slicer
 popd
 
-cp ./slicer-src/build/*.json ./slicer-out
+cp ./slicer-src/build/src/*.json ./slicer-out
