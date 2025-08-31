21bb5cbfd451445f9c828b08bcde89078fc89ef7
Javad Asgari Shafique <me@javad.sh>
2025-08-31 18:50:38 +0200

    Attemt manual workaround for CMP0167 in old case.

--- dump_slicer_steps/OrcaSlicer/install-deps.sh
+++ dump_slicer_steps/OrcaSlicer/install-deps.sh
@@ -5,6 +5,14 @@ pushd slicer-src
 if [[ -f "build_linux.sh" ]]; then
   sudo ./build_linux.sh -u
 else
+  mv CMakeLists.txt CMakeLists.txt.prev
+  cat <<EOF > CMakeLists.txt
+if(POLICY CMP0167)
+  cmake_policy(SET CMP0167 OLD)
+endif()
+EOF
+  cat CMakeLists.txt.prev >> CMakeLists.txt
+  rm CMakeLists.txt.prev
   sudo ./BuildLinux.sh -u
 fi
 
