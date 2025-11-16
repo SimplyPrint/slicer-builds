709d13f5b7b59b69fdd629fbd161448c106297bc
Ocraftyone <Ocraftyone@users.noreply.github.com>
2025-11-16 03:22:01 +0000

    Update orca run.sh

--- dump_slicer_steps/OrcaSlicer/run.sh
+++ dump_slicer_steps/OrcaSlicer/run.sh
@@ -2,7 +2,7 @@
 
 pushd slicer-src/build/package
 
-SSL_CERT_FILE=/etc/ssl/certs/ca-certificates.crt xvfb-run ./AppRun
+SSL_CERT_FILE=/etc/ssl/certs/ca-certificates.crt xvfb-run ./orca-slicer
 
 popd
 
