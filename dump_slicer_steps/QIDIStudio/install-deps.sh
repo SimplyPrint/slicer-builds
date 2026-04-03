98a0d6cdc9822252ec246c46427981b44cedcda5
Ocraftyone <Ocraftyone@users.noreply.github.com>
2026-04-03 03:54:47 +0000

    Update patches

--- dump_slicer_steps/QIDIStudio/install-deps.sh
+++ dump_slicer_steps/QIDIStudio/install-deps.sh
@@ -7,6 +7,7 @@ sudo apt-get install -y python3-numpy
 
 pushd slicer-src
 
+sed -i "s/-DQDT_RELEASE_TO_PUBLIC=1/-DQDT_RELEASE_TO_PUBLIC=0/" BuildLinux.sh 
 chmod +x ./BuildLinux.sh
 sudo ./BuildLinux.sh -u
 
