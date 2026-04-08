15622b617fddca5770ef18c5427c68d3471a6104
Johan Ohly <johanohly@gmail.com>
2026-04-08 15:00:10 +0200

    Fix ElegooSlicer dump launcher path

--- dump_slicer_steps/ElegooSlicer/run.sh
+++ dump_slicer_steps/ElegooSlicer/run.sh
@@ -1,10 +1,23 @@
 #!/bin/bash
 set -euo pipefail
 
-pushd slicer-src/build/src
+if [[ -x "slicer-src/build/package/AppRun" ]]; then
+  pushd slicer-src/build/package
 
-SSL_CERT_FILE=/etc/ssl/certs/ca-certificates.crt xvfb-run ./elegoo-slicer
+  SSL_CERT_FILE=/etc/ssl/certs/ca-certificates.crt xvfb-run ./AppRun
 
-popd
+  popd
 
-cp ./slicer-src/build/src/*.json ./slicer-out
+  cp ./slicer-src/build/package/bin/*.json ./slicer-out
+elif [[ -x "slicer-src/build/src/elegoo-slicer" ]]; then
+  pushd slicer-src/build/src
+
+  SSL_CERT_FILE=/etc/ssl/certs/ca-certificates.crt xvfb-run ./elegoo-slicer
+
+  popd
+
+  cp ./slicer-src/build/src/*.json ./slicer-out
+else
+  echo "Could not find an ElegooSlicer launcher in build/package or build/src"
+  exit 1
+fi
