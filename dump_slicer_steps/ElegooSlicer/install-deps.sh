e7ced82d3a7174f9a5f071f6865a887151673da9
Johan Ohly <johanohly@gmail.com>
2026-04-08 14:33:17 +0200

    Fix ElegooSlicer dump patching

--- dump_slicer_steps/ElegooSlicer/install-deps.sh
+++ dump_slicer_steps/ElegooSlicer/install-deps.sh
@@ -3,6 +3,9 @@ set -euo pipefail
 
 pushd slicer-src
 
+perl -0pi -e 's#https://www\.mpfr\.org/mpfr-current/mpfr-4\.2\.1\.tar\.bz2#https://www.mpfr.org/mpfr-4.2.1/mpfr-4.2.1.tar.bz2#g' deps/MPFR/MPFR.cmake
+perl -0pi -e 's#URL_HASH SHA256=c56edfacef0a60c0de3e6489194fcb2f24c03dbb550a8a7de5938642d045bd32#URL_HASH SHA256=17a3e875acece9be40b093361cfef47385d4ef22c995ffbf36b2871f5785f9b8#g' deps/TIFF/TIFF.cmake
+
 sudo apt-get install -y libgtk-3-dev
 if [[ -f "build_linux.sh" ]]; then
   ./build_linux.sh -u
