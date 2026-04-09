e950f05834ac4373243e112ad17783de505ce02e
Johan Ohly <johanohly@gmail.com>
2026-04-09 12:08:25 +0200

    fix: ElegooSlicer binary location

--- build_slicer_binaries/ElegooSlicer/package_binary.sh
+++ build_slicer_binaries/ElegooSlicer/package_binary.sh
@@ -1,12 +1,21 @@
 #!/usr/bin/env bash
 
+set -euo pipefail
+
 pushd slicer-src/build
 
 mkdir -p slicer_out/resources
 mkdir -p slicer_out/bin
 
-cp -r resources/* slicer_out/resources
-
-cp src/elegoo-slicer slicer_out/bin
+if [[ -d "package/bin" ]]; then
+  cp -r resources/* slicer_out/resources
+  cp package/bin/* slicer_out/bin
+elif [[ -x "src/elegoo-slicer" ]]; then
+  cp -r resources/* slicer_out/resources
+  cp src/elegoo-slicer slicer_out/bin
+else
+  echo "Could not find an ElegooSlicer binary in build/package/bin or build/src"
+  exit 1
+fi
 
 popd
