f0bef9ea70db5a458b4bcd4b509c24ad1a1ba7f1
Johan Ohly <johanohly@gmail.com>
2026-06-20 18:18:43 +0200

    fix(prusaslicer): package runtime libraries

--- slicers/PrusaSlicer/steps/package-binary.sh
+++ slicers/PrusaSlicer/steps/package-binary.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+set -euo pipefail
+
 pushd slicer-src/build
 
 mkdir -p slicer_out/resources
@@ -8,5 +10,10 @@ mkdir -p slicer_out/bin
 cp -r resources/* slicer_out/resources
 
 cp src/prusa-slicer slicer_out/bin
+
+ldd src/prusa-slicer \
+  | awk '/=> \// { print $3 }' \
+  | sort -u \
+  | xargs -r -I{} cp -L "{}" slicer_out/bin
 
 popd
