29aff517f2b47f135e5b5906b66bcacb4a881452
Johan Ohly <johanohly@gmail.com>
2026-06-30 11:29:11 +0200

    fix(anycubic): find packaged slicer binary

--- slicers/AnycubicSlicerNext/steps/run.sh
+++ slicers/AnycubicSlicerNext/steps/run.sh
@@ -1,10 +1,19 @@
 #!/bin/bash
 set -euo pipefail
 
-pushd slicer-src/build/package
+run_dir="slicer-src/build/package"
+if [[ ! -x "$run_dir/orca-slicer" && -x "$run_dir/bin/orca-slicer" ]]; then
+  run_dir="$run_dir/bin"
+elif [[ ! -x "$run_dir/orca-slicer" && -x "slicer-src/build/src/Release/orca-slicer" ]]; then
+  run_dir="slicer-src/build/src/Release"
+elif [[ ! -x "$run_dir/orca-slicer" && -x "slicer-src/build/src/orca-slicer" ]]; then
+  run_dir="slicer-src/build/src"
+fi
+
+pushd "$run_dir"
 
 SSL_CERT_FILE=/etc/ssl/certs/ca-certificates.crt xvfb-run ./orca-slicer
 
 popd
 
-cp ./slicer-src/build/package/bin/*.json ./slicer-out
+cp "$run_dir"/*.json ./slicer-out
